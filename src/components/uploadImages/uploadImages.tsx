eecfc5da29d97b46a555130329e1ebbc602a89e0
Dargouls <gah_500@hotmail.com>
2024-12-22 01:28:47 -0300

    fix: loading position

--- src/components/uploadImages/uploadImages.tsx
+++ src/components/uploadImages/uploadImages.tsx
@@ -129,9 +129,9 @@ interface Form2Props {
 const Form2 = ({ handleReturn, results, ...props }: Form2Props) => {
 	if (!results)
 		return (
-			<>
+			<div className='mt-16 flex h-full items-center justify-center'>
 				<CircularProgress size={40} />
-			</>
+			</div>
 		);
 	const { informacoes_imagem, imagem_processada, interpretacao, medidas_estruturais } =
 		results as ResultIrisProps;
